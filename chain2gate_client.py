d122594b1433dd426bdf73f9ed48f35be4e5dc37
Luca Massidda <lucam@iMac-di-Luca.lan>
2021-09-16 16:14:10 +0200

    logging

--- chain2gate_client.py
+++ chain2gate_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from logger import logger
 import asyncio
 import websockets
 import json
@@ -14,6 +15,7 @@ ApiKey = 'API_KEY'
 
 def configure():
     global DeviceId, RootDir, ServerIP, ApiKey
+
     with open(f"{RootDir}/settings.json", 'r') as jsonfile:
         settings = json.load(jsonfile)
         DeviceId = settings.get('DeviceId')
@@ -53,23 +55,29 @@ def upload(date=None):
     c = f'curl -X PUT "http://{ServerIP}/api/chain2gate/{DeviceId}?api_key={ApiKey}" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "file=@{RootDir}/{DeviceId}.json;type=text/plain" > /dev/null 2>&1'
     if date is not None:
         c = f'curl -X PUT "http://{ServerIP}/api/chain2gate/{DeviceId}?api_key={ApiKey}&date={date}" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "file=@{RootDir}/{DeviceId}.json;type=text/plain" > /dev/null 2>&1'
+    logger.debug(f'Uploading: {c}')
     os.system(c)
 
 async def chain2client():
     while True:
         # outer loop restarted every time the connection fails
         uri = f"ws://{DeviceId}.local:81"
+        logger.debug(f'uri: {uri}')
+        logger.debug('Reading data json file.')
         d = load_json()
+        logger.debug('Read.')
         last_upload = datetime.datetime.now()
 
         try:
             async with websockets.connect(uri) as websocket:
+                logger.info('Client started.')
                 async for message in websocket:
+                    logger.debug('Message received.')
                     msg = json.loads(message)
                     now = datetime.datetime.now()
 
                     if 'Chain2Data' in msg:
-                        print(msg)
+                        logger.info(msg)
 
                         msg_meter = msg['Chain2Data']['Meter']
                         msg_type = msg['Chain2Data']['Type']
@@ -96,29 +104,35 @@ async def chain2client():
                         
                         if msg_type == 'CF1':
                             trim_dict(d)
+                            logger.debug(f"Dictionary trimmed to {len(d['epoch'])}")
                             save_json(d)
+                            logger.info('Json file updated.')
                             upload()
-                            print('Uploaded')
+                            logger.info('Current data uploaded')
                             if last_upload.date() < now.date():
                                 upload(now.strftime("%Y%m%d"))
-                                print(f'Uploaded {now.strftime("%Y%m%d")}')
+                                logger.info(f'Daily data uploaded {now.strftime("%Y%m%d")}')
                             last_upload = now
 
                         if msg_type == 'CF1':
                             c = f"curl -i -XPOST 'http://localhost:8086/write?db=chain2gate' --data-binary '{msg_meter} energy={msg_energy} {msg_epoch}000000000' > /dev/null 2>&1"
+                            logger.debug('Sending energy data to Influxdb')
                             os.system(c)
                         
                         if msg_type == 'CF21':
                             c = f"curl -i -XPOST 'http://localhost:8086/write?db=chain2gate' --data-binary '{msg_meter} power={msg_power} {msg_epoch}000000000'  > /dev/null 2>&1"
+                            logger.debug('Sending power data to Influxdb')
                             os.system(c)
 
         except:
-            print('Socket error - retrying connection in 10 sec (Ctrl-C to quit)')
+            logger.error('Socket error - retrying connection in 10 sec (Ctrl-C to quit)')
             await asyncio.sleep(10)
             continue
 
 if __name__ == '__main__':
+    logger.debug("Program started.")
     configure()
+    logger.debug("Configuration completed.")
     asyncio.get_event_loop().run_until_complete(chain2client())
 
 
